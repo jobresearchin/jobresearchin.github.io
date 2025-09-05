63db1338e580cd8560b503f7565db539b37657da
seieric <30688855+seieric@users.noreply.github.com>
2025-09-05 11:10:46 +0900

    feat: image optimization

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,21 @@
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
   // Add the translations data
   eleventyConfig.addGlobalData("translations", () => {
     return require("./translations.js");
   });
-
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ["avif", "webp", "jpeg"],
+    widths: ["auto"],
+    htmlOptions: {
+      imgAttributes: {
+        loading: "lazy",
+        decoding: "async",
+      },
+      pictureAttributes: {}
+    }
+  });
   eleventyConfig.addPassthroughCopy("style.css");
 
   return {
