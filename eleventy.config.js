402beb5ca22b71a774cae288a0e164127cf92b59
Seiya Eriguchi <30688855+seieric@users.noreply.github.com>
2025-11-05 14:29:46 +0000

    fix: copy favicon.ico to root

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,20 +4,20 @@ const Image = require("@11ty/eleventy-img");
 
 
 async function imageShortcode(src, alt, sizes, width, height) {
-  if(alt === undefined) {
+  if (alt === undefined) {
     throw new Error(`Missing \`alt\` on image from: ${src}`);
   }
 
   const isSvg = src.toLowerCase().endsWith('.svg');
 
   let metadata = await Image(src, {
-    
+
     widths: width ? [parseInt(width, 10)] : [300, 600, 900, 1200],
-  
+
     formats: isSvg ? ['svg'] : ['webp', 'jpeg'],
     outputDir: "./_site/images/",
     urlPath: "/images/",
-    
+
     svgShortCircuit: true
   });
 
@@ -34,18 +34,19 @@ async function imageShortcode(src, alt, sizes, width, height) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // (他の設定はそのまま)
   eleventyConfig.addGlobalData("translations", () => {
     return require("./translations.js");
   });
-  
+
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("static");
-  eleventyConfig.addPassthroughCopy("images"); 
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 
   return {
     dir: {
